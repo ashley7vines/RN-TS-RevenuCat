306a92692622c2cbb4995b425bb067a07e32a091
ashleyvines <AshleyVinesDev@gmail.com>
2024-06-26 11:05:02 -0500

    feat: update App.tsx to integrate react-native-purchases

--- App.tsx
+++ App.tsx
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { Platform, Text, View, StyleSheet } from "react-native";
+
+import Purchases, {
+  LOG_LEVEL,
+  PurchasesOffering,
+} from "react-native-purchases";
+
+const APIKeys = {
+  apple: "your_revenuecat_apple_api_key",
+  google: "your_revenuecat_google_api_key",
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [currentOffering, setCurrentOffering] =
+    useState<PurchasesOffering | null>(null);
+
+  useEffect(() => {
+    const setup = async () => {
+      if (Platform.OS == "android") {
+        await Purchases.configure({ apiKey: APIKeys.google });
+      } else {
+        await Purchases.configure({ apiKey: APIKeys.apple });
+      }
+
+      const offerings = await Purchases.getOfferings();
+      setCurrentOffering(offerings.current);
+    };
+
+    Purchases.setLogLevel(LOG_LEVEL.DEBUG);
+
+    setup().catch(console.log);
+  }, []);
+
+  if (!currentOffering) {
+    return "Loading...";
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>Current Offering: {currentOffering.identifier}</Text>
+        <Text style={{ marginTop: 10 }}>
+          Package Count: {currentOffering.availablePackages.length}
+        </Text>
+        <View style={{ marginTop: 10 }}>
+          {currentOffering.availablePackages.map((pkg) => {
+            return (
+              <Text style={{ marginTop: 10 }}>{pkg.product.identifier}</Text>
+            );
+          })}
+        </View>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
